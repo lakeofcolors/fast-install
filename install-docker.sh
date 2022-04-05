fc50c587fcfa5beb2130cb20706a7bcb0698036a
Vladimir Kochetkov <vladimirkochetkov@Vladimirs-MacBook-Pro.local>
2022-04-05 07:31:00 +0700

    forgot some :)

--- install-docker.sh
+++ install-docker.sh
@@ -16,7 +16,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 apt-cache policy docker-ce
-
+sudo apt install docker-ce
 sleep 1
 sudo systemctl status docker
 sudo usermod -aG docker ${USER}
